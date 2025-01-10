8221ac775dc6bd3bd38e57e7e8695a40d751dafe
niraj <niraj@mapmyindia.com>
2025-01-10 15:48:40 +0530

    add basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+
+
+const nzZorro = [
+  NzLayoutModule
+]
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -10,9 +25,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ...nzZorro
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US },
+    provideAnimationsAsync(),
+    provideHttpClient()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
